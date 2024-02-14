fa712beb84337a65365bb7ef6e9b5b122d88aa78
LaniKN <nohealani2022@gmail.com>
2024-02-14 14:29:16 -0500

    Added all majors w ids json file to db

---
+++ SqliteDB/database/table_info/majorData.js
@@ -0,0 +1,271 @@
+
+[
+    {
+      "Major": " Administration",
+      "ID": 1
+    },
+    {
+      "Major": " Applied Exercise",
+      "ID": 2
+    },
+    {
+      "Major": " Biomedical",
+      "ID": 3
+    },
+    {
+      "Major": " Broadcasting",
+      "ID": 4
+    },
+    {
+      "Major": " Business Administration",
+      "ID": 5
+    },
+    {
+      "Major": " Business Communication",
+      "ID": 6
+    },
+    {
+      "Major": " Cellular",
+      "ID": 7
+    },
+    {
+      "Major": " Chemistry",
+      "ID": 8
+    },
+    {
+      "Major": " Child",
+      "ID": 9
+    },
+    {
+      "Major": " Communication",
+      "ID": 10
+    },
+    {
+      "Major": " Computer",
+      "ID": 11
+    },
+    {
+      "Major": " Computer Engineering",
+      "ID": 12
+    },
+    {
+      "Major": " Computer Science",
+      "ID": 13
+    },
+    {
+      "Major": " Conditioning",
+      "ID": 14
+    },
+    {
+      "Major": " Conflict",
+      "ID": 15
+    },
+    {
+      "Major": " Construction Management",
+      "ID": 16
+    },
+    {
+      "Major": " Criminal",
+      "ID": 17
+    },
+    {
+      "Major": " Criminal Justice",
+      "ID": 18
+    },
+    {
+      "Major": " Cybersecurity",
+      "ID": 19
+    },
+    {
+      "Major": " Data Analytics",
+      "ID": 20
+    },
+    {
+      "Major": " Defense Financial",
+      "ID": 21
+    },
+    {
+      "Major": " Electrical",
+      "ID": 22
+    },
+    {
+      "Major": " Electrical Engineering",
+      "ID": 23
+    },
+    {
+      "Major": " Emergency Response",
+      "ID": 24
+    },
+    {
+      "Major": " Engineering",
+      "ID": 25
+    },
+    {
+      "Major": " Entrepreneurial Studies",
+      "ID": 26
+    },
+    {
+      "Major": " Exercise Science",
+      "ID": 27
+    },
+    {
+      "Major": " Financial Services",
+      "ID": 28
+    },
+    {
+      "Major": " Forensic",
+      "ID": 29
+    },
+    {
+      "Major": " General",
+      "ID": 30
+    },
+    {
+      "Major": " Graphic Design",
+      "ID": 31
+    },
+    {
+      "Major": " Health",
+      "ID": 32
+    },
+    {
+      "Major": " Health Care",
+      "ID": 33
+    },
+    {
+      "Major": " Health Information",
+      "ID": 34
+    },
+    {
+      "Major": " Health Science",
+      "ID": 35
+    },
+    {
+      "Major": " Human",
+      "ID": 36
+    },
+    {
+      "Major": " Human Performance",
+      "ID": 37
+    },
+    {
+      "Major": " Human Resources",
+      "ID": 38
+    },
+    {
+      "Major": " Industrial",
+      "ID": 39
+    },
+    {
+      "Major": " Information",
+      "ID": 40
+    },
+    {
+      "Major": " Information Systems",
+      "ID": 41
+    },
+    {
+      "Major": " Journalism",
+      "ID": 42
+    },
+    {
+      "Major": " Legal",
+      "ID": 43
+    },
+    {
+      "Major": " Management",
+      "ID": 44
+    },
+    {
+      "Major": " Manufacturing",
+      "ID": 45
+    },
+    {
+      "Major": " Manufacturing Engineering",
+      "ID": 46
+    },
+    {
+      "Major": " Marketing",
+      "ID": 47
+    },
+    {
+      "Major": " Mechanical",
+      "ID": 48
+    },
+    {
+      "Major": " Mechanical Engineering",
+      "ID": 49
+    },
+    {
+      "Major": " Mechatronics",
+      "ID": 50
+    },
+    {
+      "Major": " Medical",
+      "ID": 51
+    },
+    {
+      "Major": " Molecular Biology",
+      "ID": 52
+    },
+    {
+      "Major": " Network",
+      "ID": 53
+    },
+    {
+      "Major": " Physiology",
+      "ID": 54
+    },
+    {
+      "Major": " Production Management",
+      "ID": 55
+    },
+    {
+      "Major": " Professional",
+      "ID": 56
+    },
+    {
+      "Major": " Quality Management",
+      "ID": 57
+    },
+    {
+      "Major": " Recreation",
+      "ID": 58
+    },
+    {
+      "Major": " Rehabilitative Services",
+      "ID": 59
+    },
+    {
+      "Major": " Risk Management",
+      "ID": 60
+    },
+    {
+      "Major": " Robotics",
+      "ID": 61
+    },
+    {
+      "Major": " Software",
+      "ID": 62
+    },
+    {
+      "Major": " Sport",
+      "ID": 63
+    },
+    {
+      "Major": " Sports Media",
+      "ID": 64
+    },
+    {
+      "Major": " Strength",
+      "ID": 65
+    },
+    {
+      "Major": " Supply Chain",
+      "ID": 66
+    },
+    {
+      "Major": "Accounting",
+      "ID": 67
+    }
+  ]
