06bcde5527960db6c961a5afe4b63d4b4cf2da76
Abraham Dagnew <Ahaymero@gmail.com>
2024-04-12 16:56:48 -0400

    Revert "Nick's changes"
    This reverts commit ef816f752823be5748ab91f5289f739fa3d730ee.

--- SchedulingWebApp/Controller/BulkInsert.cs
+++ SchedulingWebApp/Controller/BulkInsert.cs
@@ -11,17 +11,12 @@ using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using SchedulingWebApp.Controller.Interfaces;
 using Microsoft.Data.Sqlite;
-using System.Text.Json.Serialization;
-using Microsoft.VisualBasic;
-using SchedulingWebApp.Data.Model;
-using System.Text.Json;
-using System.Data.SqlTypes;
 
 namespace SchedulingWebApp.Controller.DapperDbConnection;
     public class DataContext : HomeController
     {
         public readonly IConfiguration Configuration;
-        private readonly FileStream fs = new FileStream("courseData.js", FileMode.Open);
+
         public DataContext(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -31,36 +26,8 @@ namespace SchedulingWebApp.Controller.DapperDbConnection;
         {
             return new SqliteConnection("Data Source = Courses.db");
         }
-        public void insertAll(IDbConnection connection) {
-            var json = File.ReadAllText("courseData.js");
-            var data = JsonSerializer.Deserialize<List<Course>>(json);
-            if (data is null) {
-                return;
-            }
-            foreach (Course course in data) {
-                InsertCourse(connection, course);
-            }
-        }
 
-        public void InsertCourse(IDbConnection connection, Course course) {
-            string sqlString = $"INSERT INTO COURSE(CourseOID, CourseCode, Name, PreReqs) VALUES(${course.CourseOID},${course.CourseCode}, ${course.Name}, ${course.PreReqs})";
-            
-            // SqliteCommand command = new SqliteCommand(sqlString,connection)
-            // check to finish up here. Josh sent you the "stuff"
-            int rowsAffected = connection.Execute(sqlString, new { MajorName = MajorName, CourseID = CourseID });
-
-            if (rowsAffected > 0)
-            {
-                Console.WriteLine("Pair inserted successfully!");
-            }
-            else
-            {
-                Console.WriteLine("Failed to insert pair.");
-            }
-        }
-    
-
-        public void Init(string MajorName, int CourseID)
+        public void Init(string[] MajorName, int[] CourseID)
         {
             using (var connection = CreateConnection())
             {
@@ -83,13 +50,12 @@ namespace SchedulingWebApp.Controller.DapperDbConnection;
             connection.Execute(createPairsTableSql);
         }
        
-        private void InsertPair(IDbConnection connection, string MajorName, int CourseID)
+        private void InsertPair(IDbConnection connection, string[] MajorName, int[] CourseID)
         {
             string insertQuery = @"
                 INSERT INTO Pairs (MajorName, CourseID)
-                VALUES (" + MajorName + ", " + CourseID + @");
+                VALUES (@MajorName, @CourseID);
             ";
-
 
             int rowsAffected = connection.Execute(insertQuery, new { MajorName = MajorName, CourseID = CourseID });
 
@@ -103,7 +69,7 @@ namespace SchedulingWebApp.Controller.DapperDbConnection;
             }
         }
 
-        public void UpdatePair(IDbConnection connection, string MajorName, int CourseID)
+        public void UpdatePair(IDbConnection connection, string[] MajorName, int[] CourseID)
         {
             string updateQuery = @"
                 UPDATE Pairs 
@@ -124,7 +90,7 @@ namespace SchedulingWebApp.Controller.DapperDbConnection;
             } 
         }
 
-        public void DeletePair(IDbConnection connection, string MajorName, int CourseID)
+        public void DeletePair(IDbConnection connection, string[] MajorName, int[] CourseID)
         {
             string deleteQuery = @"
                 DELETE FROM Pairs 
