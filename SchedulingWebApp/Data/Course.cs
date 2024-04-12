0bb206be76893245fa7ff1152250bdffaf83c8f8
Abraham Dagnew <Ahaymero@gmail.com>
2024-04-12 17:00:26 -0400

    Reapply "Nick's changes"
    This reverts commit 06bcde5527960db6c961a5afe4b63d4b4cf2da76.

--- SchedulingWebApp/Data/Course.cs
+++ SchedulingWebApp/Data/Course.cs
@@ -1,15 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Web;
 
 namespace SchedulingWebApp.Data.Model;
 
 	public class Course
 	{
+		[JsonPropertyName("CourseOID")]
 		public int CourseOID { get; set; }
+		[JsonPropertyName("CourseCode")]
 		public string? CourseCode { get; set; }
+		[JsonPropertyName("Name")]
 		public string? Name { get; set; }
+		[JsonPropertyName("PreReqs")]
 		public string? PreReqs { get; set; }
 
 		
