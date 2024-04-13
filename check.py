450d3141b206e0d6bd32b92965de3879c7b113e1
LaniKN <nohealani2022@gmail.com>
2024-04-12 21:38:15 -0400

    Fixed null json obj, py script to check dups

---
+++ check.py
@@ -0,0 +1,28 @@
+# Just run a cute little python check.py in the terminal if you have python installed
+# and this should run ;) 923 total course/objs in the json file
+import json
+
+input_file = open ('./SqliteDB/database/table_info/courseData.js')
+json_arr = json.load(input_file)
+
+courses_list = []
+dupCourses = []
+uniqueCourses= []
+
+for item in json_arr:
+    courseCode = {"CourseCode:":None}
+    courseCode['CourseCode:'] = item["CourseCode:"]
+    courses_list.append(courseCode)
+
+# print(courses_list)
+
+for pair in courses_list:
+    for course in pair.items():
+        if pair not in uniqueCourses:
+            uniqueCourses.append(course)
+        else:
+            dupCourses.append(course)
+
+print(dupCourses)
+print(len(uniqueCourses))
+print(len(courses_list))
